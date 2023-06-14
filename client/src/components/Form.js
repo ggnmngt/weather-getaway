c5e9b7ed04b247fea08c698a239585c8f10e6325
Gagan <ggnmngt@gmail.com>
2023-06-14 11:33:51 -0400

    updated the client to show only month as input

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,10 +2,7 @@ import React, { useState } from 'react';
 import './Form.css';
 
 const Form = ({ onSearch }) => {
-  const [startMonth, setStartMonth] = useState('');
-  const [startDay, setStartDay] = useState('');
-  const [endMonth, setEndMonth] = useState('');
-  const [endDay, setEndDay] = useState('');
+  const [travelMonth, setTravelMonth] = useState('');
   const [minTemp, setMinTemp] = useState('');
   const [maxTemp, setMaxTemp] = useState('');
 
@@ -23,8 +20,7 @@ const Form = ({ onSearch }) => {
 
     // Prepare search data object
     const searchData = {
-      startDate: `${startMonth}-${startDay}`,
-      endDate: `${endMonth}-${endDay}`,
+      month: travelMonth,
       minTemp,
       maxTemp,
     };
@@ -34,7 +30,7 @@ const Form = ({ onSearch }) => {
 
   const validateInputs = () => {
     // Perform validation logic here
-    // You can check the values of startDate, endDate, minTemp, maxTemp and return false if any validation condition fails
+    // Check the values of travleMonth, minTemp, maxTemp and return false if any validation condition fails
   
     return true; // Return true if all validation conditions pass
   };
@@ -45,11 +41,11 @@ const Form = ({ onSearch }) => {
         <h1 className="Form-title">City Search</h1>
         <form onSubmit={handleSubmit}>
           <div className="Form-row">
-            <label htmlFor="start-month">Start Month:</label>
+            <label htmlFor="travel-month">Travel Month:</label>
             <select
-              id="start-month"
-              value={startMonth}
-              onChange={(e) => setStartMonth(e.target.value)}
+              id="travel-month"
+              value={travelMonth}
+              onChange={(e) => setTravelMonth(e.target.value)}
               required
             >
               <option value="">Select Month</option>
@@ -57,36 +53,6 @@ const Form = ({ onSearch }) => {
                 <option key={month} value={month}>{month}</option>
               ))}
             </select>
-            <label htmlFor="start-day">Start Day:</label>
-            <input
-              type="text"
-              id="start-day"
-              value={startDay}
-              onChange={(e) => setStartDay(e.target.value)}
-              required
-            />
-          </div>
-          <div className="Form-row">
-            <label htmlFor="end-month">End Month:</label>
-            <select
-              id="end-month"
-              value={endMonth}
-              onChange={(e) => setEndMonth(e.target.value)}
-              required
-            >
-              <option value="">Select Month</option>
-              {months.map((month) => (
-                <option key={month} value={month}>{month}</option>
-              ))}
-            </select>
-            <label htmlFor="end-day">End Day:</label>
-            <input
-              type="text"
-              id="end-day"
-              value={endDay}
-              onChange={(e) => setEndDay(e.target.value)}
-              required
-            />
           </div>
           <div className="Form-row">
             <label htmlFor="min-temp">Min Temperature:</label>
